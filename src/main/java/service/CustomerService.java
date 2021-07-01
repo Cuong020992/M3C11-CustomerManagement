e626429e04c4c3d2ead41b072246c97938e0f89e
cuong0209992 <sandtimezone92@gmail.com>
2021-07-01 14:35:56 +0700

    M3C11 CustomerManagement

---
+++ src/main/java/service/CustomerService.java
@@ -0,0 +1,18 @@
+package service;
+
+import java.util.List;
+
+import model.Customer;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
+
